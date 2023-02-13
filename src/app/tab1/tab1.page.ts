9b6c45ce0acaf7701f4ed6dec9b3b3ffe2246fe4
Leonardo Alves <leonardoalves72@hotmail.com>
2023-02-13 20:25:40 -0300

    animação para girar a moeda
    import angular animations

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,25 +1,41 @@
+import { animate, state, style, transition, trigger } from '@angular/animations';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  styleUrls: ['tab1.page.scss'],
+  animations: [
+    trigger('rotatedState', [
+      state('default', style({ transform: 'rotateY(0)' })),
+      state('rotated', style({ transform: 'rotateY(-360deg)' })),
+      transition('rotated => default', animate('2000ms ease-out')),
+      transition('default => rotated', animate('2000ms ease-in')),
+    ])
+  ],
 })
 export class Tab1Page {
   moeda = 'assets/logo.png';
   cara = 'assets/cara.png';
   coroa = 'assets/coroa.png';
   info = 'Clique em "Jogar a moeda!"' // PODE DAR RUIM as aspas dupla
+  state = 'default';
 
-  constructor() {}
+  constructor() { }
 
-  jogarMoeda(){
-    if(Math.random() < 0.5){
-      this.moeda = this.cara;
-      this.info = 'Cara!';
-    } else {
-      this.moeda = this.coroa;
-      this.info = 'Coroa!';
-    }
+  jogarMoeda() {
+    this.moeda = 'assets/logo.png';
+    this.info = 'girando...';
+    this.state = (this.state === 'default') ? 'rotated' : 'default';
+
+    setTimeout(() => {
+      if (Math.random() < 0.5) {
+        this.moeda = this.cara;
+        this.info = 'Cara!';
+      } else {
+        this.moeda = this.coroa;
+        this.info = 'Coroa!';
+      }
+    }, 2000);
   }
 }
